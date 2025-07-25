f98d2757e51d306913491e2ca8a7b98ee914e536
john maina <maina202120@gmail.com>
2025-07-25 23:58:06 +0300

    submit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router"; // use the full router created with createBrowserRouter
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,16 +1,36 @@
+
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/actors")
+      .then((res) => res.json())
+      .then(setActors);
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <article key={actor.name}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies?.map((m, i) => (
+                <li key={i}>{m}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Actors;
